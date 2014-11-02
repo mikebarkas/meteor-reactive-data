ea7e25faafa0566da425e0a53af92c9bdbe4135f
Mike Barkas <mike@barkas.com>
2014-11-02 15:41:09 -0500

    js exercise file.

--- meteor-reactive-data.js
+++ meteor-reactive-data.js
@@ -1,23 +1,17 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+    Name = {
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+    };
+
+    printName = function () {
+        var name = Name.get();
+
+    };
+
+    Deps.autorun(function () {
+        printName();
+    });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+
